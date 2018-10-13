2b016e4722655a561d8d6a96d8eea6408cea65c7
Your Name <you@example.com>
2018-10-13 09:49:40 -0300

    python client

--- app-c/app/web_app.py
+++ app-c/app/web_app.py
@@ -9,6 +9,13 @@ import tornado.web
 def create_web_app():
     return tornado.web.Application([
         (r"/status", StatusHandler, {'foo': 'bah'}),
+
+        # ultima consulta
+        # movimentacao financeira
+        # dados da ultima compra
+
+        # post globalzao
+
         # (r"/backend/resultado", ResultadoHandler, {'redis': redis_client}),
         # (r"/backend/histograma", HistogramaHandler, {'redis': redis_client}),
     ])
